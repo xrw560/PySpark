27ea32167fe0422de67f996195ffc88b87a28c51
zhouning <ncutits@163.com>
2018-07-16 09:58:13 +0800

    tensorflow python3 建立自己的网络

---
+++ basic_python/basic/log_demo/config_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- encoding:utf-8 -*-
+"""
+@author: zhouning
+@file:config_test.py
+@time:2018/7/13 11:53
+@desc:
+"""
---
+++ basic_python/basic/log_demo/demo1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- encoding:utf-8 -*-
+"""
+@author: zhouning
+@file:demo1.py
+@time:2018/7/13 9:38
+@desc:
+"""
