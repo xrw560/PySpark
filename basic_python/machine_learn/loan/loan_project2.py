3feac640bf07797a610d221410b40b16d91539af
ncutits <ncutits@163.com>
2018-07-18 21:47:15 +0800

    贷款申请最大化利润

---
+++ basic_python/machine_learn/loan/loan_project2.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+loans = pd.read_csv("cleaned_loans2007.csv")
+# print(loans.info())
+
+"""逻辑回归(二分类)"""
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression()
+cols = loans.columns
+train_cols = cols.drop("loan_status")
+features = loans[train_cols]
+target = loans["loan_status"]
+lr.fit(features, target)
+predictions = lr.predict(features)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import cross_val_predict, KFold
+
+lr = LogisticRegression()
+kf = KFold(features.shape[0], random_state=1)
+predictions = cross_val_predict(lr, features, target, cv=kf)
+predictions = pd.Series(predictions)
+
+# False positives.
+fp_filter = (predictions == 1) & (loans["loan_status"] == 0)
+fp = len(predictions[fp_filter])
+
+# True positives.
+tp_filter = (predictions == 1) & (loans["loan_status"] == 1)
+tp = len(predictions[tp_filter])
+
+# False negatives.
+fn_filter = (predictions == 0) & (loans["loan_status"] == 1)
+fn = len(predictions[fn_filter])
+
+# True negatives
+tn_filter = (predictions == 0) & (loans["loan_status"] == 0)
+tn = len(predictions[tn_filter])
+
+# Rates
+tpr = tp / float((tp + fn))
+fpr = fp / float((fp + tn))
+
+print(tpr)
+print(fpr)
+print(predictions[:20])
