f72e432e82861a670814645a1be82f04e81ed5c8
zhouning <ncutits@163.com>
2018-07-30 17:43:36 +0800

    pyspark jdbc

---
+++ spark/sql/jdbc_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- encoding:utf-8 -*-
+"""
+@author: zhouning
+@file:jdbc_test.py
+@time:2018/7/30 16:34
+@desc:
+"""
+from pyspark import SparkConf, SparkContext
+import os
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("jdbc_test") \
+    .getOrCreate()
+url = "jdbc:mysql://192.168.2.179:3306/bigdata_test"
+table = "(select evaluate_time_length from script_metadata) t"
+properties = {
+    "user": "root",
+    "password": "root",
+    "driver": "com.mysql.jdbc.Driver"
+}
+
+df = spark.read.jdbc(url, table, properties=properties)
+print(df.schema)
+df.show()
