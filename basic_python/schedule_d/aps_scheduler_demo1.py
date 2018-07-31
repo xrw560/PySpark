cc3bb457b182df7f2562b30f12d2a1ea76128a14
zhouning <ncutits@163.com>
2018-07-31 09:26:13 +0800

    apsscheduler调度

---
+++ basic_python/schedule_d/aps_scheduler_demo1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- encoding:utf-8 -*-
+"""
+@author: zhouning
+@file:aps_scheduler_demo1.py
+@time:2018/7/30 19:32
+@desc:
+"""
---
+++ basic_python/schedule_d/apscheduler_demo2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- encoding:utf-8 -*-
+"""
+@author: zhouning
+@file:apscheduler_demo2.py
+@time:2018/7/30 19:42
+@desc:
+"""
