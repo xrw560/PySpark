1dc8ff5067326e58c3c54bd746c23b6c16f61cdf
zhouning <ncutits@163.com>
2018-07-31 09:26:44 +0800

    调度

--- basic_python/schedule_d/aps_scheduler_demo1.py
+++ basic_python/schedule_d/aps_scheduler_demo1.py
@@ -4,5 +4,29 @@
 @author: zhouning
 @file:aps_scheduler_demo1.py
 @time:2018/7/30 19:32
-@desc:
+@desc:APScheduler是基于Quartz的一个Python定时任务框架。提供了基于日期、固定时间间隔以及crontab类型的任务，并且可以持久化任务。
 """
+from datetime import datetime
+import time
+import os
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def tick():
+    print("Tick! The time is: %s " % datetime.now())
+
+
+if __name__ == '__main__':
+    scheduler = BackgroundScheduler()
+    # 间隔3秒钟执行一次
+    scheduler.add_job(tick, 'interval', seconds=3)
+    # 这里的调度任务是独立的一个线程
+    scheduler.start()
+    print("Press Ctrl+{0} to exit.".format('Break' if os.name == 'nt' else 'C'))
+    try:
+        while True:
+            time.sleep(2)
+            print('sleep!')
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
+        print("Exit The Job!")
--- basic_python/schedule_d/apscheduler_demo2.py
+++ basic_python/schedule_d/apscheduler_demo2.py
@@ -5,4 +5,45 @@
 @file:apscheduler_demo2.py
 @time:2018/7/30 19:42
 @desc:
+https://www.cnblogs.com/qq1207501666/p/9195186.html
 """
+
+from apscheduler.schedulers.background import BackgroundScheduler, BlockingScheduler
+from apscheduler.jobstores.redis import RedisJobStore
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+
+
+def print_args(*args):
+    """要定时执行的函数"""
+    for arg in args:
+        print(arg)
+
+
+# 执行器，常用的就线程池和进程池两种
+thread_pool = ThreadPoolExecutor(30)
+process_pool = ProcessPoolExecutor(5)
+executors = {
+    'thread': thread_pool,
+    'process': process_pool
+}
+
+#存储器 默认使用内存，对定时任务丢失什么的不敏感，对定时任务执行要求低
+redis_store = RedisJobStore(host="172.16.120.120",port='6379')
+sqlite_store = SQLAlchemyJobStore(url= 'sqlite:///jobs.sqlite')
+jobstores = {
+    'redis':redis_store,
+    'default':sqlite_store
+}
+#删除被持久化的定时任务， redis_store.remove_all_jobs()
+
+#调度器
+#调度器设置
+job_defaults = {
+    'coalesce':False,
+    'max_instances':5,
+    'misfire_grace_time':60
+}
+
+
+
