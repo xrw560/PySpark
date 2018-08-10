7478b14c1c382ce8addfe07c34c9404127165db1
ncutits@163.com <ncutits@163.com>
2018-08-10 23:35:34 +0800

    爬虫1

--- basic_python/web_crawler/web_parse.py
+++ basic_python/web_crawler/web_parse.py
@@ -1,8 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://cn.tripadvisor.com/Attractions-g60763-Activities-New_York_City_New_York.html'
-wb_data = requests.get(url)
-soup = BeautifulSoup(wb_data.text, 'lxml')
-imgs = soup.select('img')
-print(imgs)
+response = requests.get(url='https://www.autohome.com.cn/news/')
+response.encoding = response.apparent_encoding
+soup = BeautifulSoup(response.text, features="html.parser")
+target = soup.find(id='auto-channel-lazyload-article')
+
+li_list = target.find_all("li")
+for li in li_list:
+    a = li.find("a")
+    if a:
+        print(a.attrs.get('href'))
+        txt = a.find("h3").text
+        print(txt)
+        img = a.find("img")
+        img_url = img.attrs.get("src")
+        print(img_url[2:])
+
+        img_response = requests.get(url="https://"+img_url[2:])
+        import uuid
+
+        file_name = str(uuid.uuid4())+".jpg"
+        with open(file_name, 'wb') as f:
+            f.write(img_response.content)
+
+
+
